ac9d67272cf854e21cc52c546f38916d914f4328
Russell Porter <russell.porter@scout24.com>
2020-03-25 22:01:32 +0900

    Add tests for centralPointsInFeatures

--- src/transforms/GeoTransforms.unit.test.ts
+++ src/transforms/GeoTransforms.unit.test.ts
@@ -1,7 +1,123 @@
-import { featureCollection, lineString, point } from "@turf/helpers";
-import { polygonEnclosing } from "./GeoTransforms";
+import {
+  featureCollection,
+  lineString,
+  multiPolygon,
+  point,
+  polygon
+} from "@turf/helpers";
+import { centralPointsInFeature, polygonEnclosing } from "./GeoTransforms";
 
 describe("GeoTransforms", () => {
+  describe("centralPointsInFeatures", () => {
+    it("should provide same output for point input", () => {
+      expect(centralPointsInFeature(point([0, 0]).geometry))
+        .toMatchInlineSnapshot(`
+        Object {
+          "coordinates": Array [
+            0,
+            0,
+          ],
+          "type": "Point",
+        }
+      `);
+    });
+
+    // NOTE: result should be 0.5, 0.5, it's a bug in turf.js.
+    // https://github.com/Turfjs/turf/issues/1762
+    it("should provide point in polygon", () => {
+      expect(
+        centralPointsInFeature(
+          polygon([
+            [
+              [0, 0],
+              [0, 1],
+              [1, 1],
+              [1, 0],
+              [0, 0]
+            ]
+          ]).geometry
+        )
+      ).toMatchInlineSnapshot(`
+        Object {
+          "coordinates": Array [
+            0.4,
+            0.4,
+          ],
+          "type": "Point",
+        }
+      `);
+    });
+
+    it("should provide point inside polygon that is not in the hole", () => {
+      expect(
+        centralPointsInFeature(
+          polygon([
+            [
+              [0, 0],
+              [0, 1],
+              [1, 1],
+              [1, 0],
+              [0, 0]
+            ],
+            [
+              [0.25, 0.25],
+              [0.75, 0.25],
+              [0.75, 0.75],
+              [0.25, 0.75],
+              [0.25, 0.25]
+            ]
+          ]).geometry
+        )
+      ).toMatchInlineSnapshot(`
+        Object {
+          "coordinates": Array [
+            0.24999999999999997,
+            0.4249911804562593,
+          ],
+          "type": "Point",
+        }
+      `);
+    });
+
+    it("should provide multiple points in multipolygon", () => {
+      const firstShape: GeoJSON.Polygon["coordinates"] = [
+        [
+          [0, 0],
+          [0, 1],
+          [1, 1],
+          [1, 0],
+          [0, 0]
+        ]
+      ];
+      const secondShape: GeoJSON.Polygon["coordinates"] = [
+        [
+          [2, 2],
+          [2, 3],
+          [3, 3],
+          [3, 2],
+          [2, 2]
+        ]
+      ];
+
+      expect(
+        centralPointsInFeature(multiPolygon([firstShape, secondShape]).geometry)
+      ).toMatchInlineSnapshot(`
+        Object {
+          "coordinates": Array [
+            Array [
+              0.4,
+              0.4,
+            ],
+            Array [
+              2.4,
+              2.4,
+            ],
+          ],
+          "type": "MultiPoint",
+        }
+      `);
+    });
+  });
   describe("polygonEnclosing", () => {
     it("should generate a polygon that encloses a feature", () => {
       expect(polygonEnclosing(featureCollection([point([0, 0])])))
