2cbde18df57db8710261636698e641ef8e35edab
Russell Porter <russell.porter@autoscout24.com>
2021-02-08 23:23:41 +0100

    Remove now redundant type definitions

--- types/@turf/union/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Feature, MultiPolygon, Polygon, Properties } from "@turf/helpers";
-
-export default function union<P = Properties>(
-  polygon1: Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon,
-  polygon2: Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon,
-  options?: { properties?: P }
-): Feature<Polygon | MultiPolygon, P>;
