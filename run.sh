d5069d2e7fda57c8c8f44ce45e035731b7ba62ab
Russell Porter <contact@russellporter.com>
2023-05-21 07:59:00 +0200

    Use newer docker compose

--- run.sh
+++ run.sh
@@ -13,36 +13,36 @@ fi
 
 if [ -z "$CLUSTERING_ARANGODB_URL" ]; then
 		echo "Starting clustering DB..."
-    docker-compose up -d
-    CLUSTERING_ARANGODB_URL="http://$(docker-compose port arangodb 8529)"
+    docker compose up -d
+    CLUSTERING_ARANGODB_URL="http://$(docker compose port arangodb 8529)"
 fi
 
 echo "Converting to GeoJSON..."
 GEOCODING_SERVER_URL="https://photon.komoot.io/reverse" CLUSTERING_ARANGODB_URL=$CLUSTERING_ARANGODB_URL npm run prepare-geojson
 
-docker-compose run tippecanoe \
+docker compose run tippecanoe \
   tippecanoe -o /data/planet_lifts.mbtiles \
 		-f -z 15 -Z 5 --simplify-only-low-zooms \
 		--drop-densest-as-needed \
 		--named-layer=lifts:/data/mapboxgl_lifts.geojson;
 
-docker-compose run tippecanoe \
+docker compose run tippecanoe \
   tippecanoe -o /data/planet_runs.mbtiles \
 		-f -z 15 -Z 9 --simplify-only-low-zooms \
 		--drop-densest-as-needed \
 	  --named-layer=runs:/data/mapboxgl_runs.geojson;
 
-docker-compose run tippecanoe \
+docker compose run tippecanoe \
   tippecanoe -o /data/ski_areas.mbtiles \
 		-f -z 15 -Z 0 -B 0 \
 		--named-layer=skiareas:/data/mapboxgl_ski_areas.geojson;
 
-docker-compose run tippecanoe \
+docker compose run tippecanoe \
   tile-join -f --no-tile-size-limit -o /data/openskimap.mbtiles /data/ski_areas.mbtiles /data/planet_runs.mbtiles /data/planet_lifts.mbtiles;
 
 rm -Rf data/openskimap/*
-docker-compose run tippecanoe \
+docker compose run tippecanoe \
   tile-join -e /data/openskimap/ /data/openskimap.mbtiles;
 
-docker-compose down
+docker compose down
 
