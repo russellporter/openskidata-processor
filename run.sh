d9441885e0807c3d9828ad9b6fbb4946a2040473
Russell Porter <contact@russellporter.com>
2023-07-24 18:37:49 +0200

    Clean up container after running tippecanoe

--- run.sh
+++ run.sh
@@ -20,28 +20,28 @@ fi
 echo "Converting to GeoJSON..."
 GEOCODING_SERVER_URL="https://photon.komoot.io/reverse" CLUSTERING_ARANGODB_URL=$CLUSTERING_ARANGODB_URL npm run prepare-geojson
 
-docker compose run tippecanoe \
+docker compose run --rm tippecanoe \
   tippecanoe -Q -o /data/planet_lifts.mbtiles \
 		-f -z 15 -Z 5 --simplify-only-low-zooms \
 		--drop-densest-as-needed \
 		--named-layer=lifts:/data/mapboxgl_lifts.geojson;
 
-docker compose run tippecanoe \
+docker compose run --rm tippecanoe \
   tippecanoe -Q -o /data/planet_runs.mbtiles \
 		-f -z 15 -Z 9 --simplify-only-low-zooms \
 		--drop-densest-as-needed \
 	  --named-layer=runs:/data/mapboxgl_runs.geojson;
 
-docker compose run tippecanoe \
+docker compose run --rm tippecanoe \
   tippecanoe -Q -o /data/ski_areas.mbtiles \
 		-f -z 15 -Z 0 -B 0 \
 		--named-layer=skiareas:/data/mapboxgl_ski_areas.geojson;
 
-docker compose run tippecanoe \
+docker compose run --rm tippecanoe \
   tile-join -f --no-tile-size-limit -o /data/openskimap.mbtiles /data/ski_areas.mbtiles /data/planet_runs.mbtiles /data/planet_lifts.mbtiles;
 
 rm -Rf data/openskimap/*
-docker compose run tippecanoe \
+docker compose run --rm tippecanoe \
   tile-join -e /data/openskimap/ /data/openskimap.mbtiles;
 
 docker compose down
