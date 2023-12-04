7f504973a82f25dd0177b84b1895b5835d310a84
Russell Porter <contact@russellporter.com>
2023-12-04 21:36:40 +0100

    Serialize prepare steps

--- src/PrepareGeoJSON.ts
+++ src/PrepareGeoJSON.ts
@@ -3,20 +3,20 @@ import merge from "merge2";
 import { FeatureType } from "openskidata-format";
 import { Readable } from "stream";
 import StreamToPromise from "stream-to-promise";
-import clusterSkiAreas from "./clustering/ClusterSkiAreas";
 import { Config } from "./Config";
+import clusterSkiAreas from "./clustering/ClusterSkiAreas";
 import { DataPaths, getPath } from "./io/GeoJSONFiles";
 import { readGeoJSONFeatures } from "./io/GeoJSONReader";
-import { RunNormalizerAccumulator } from "./transforms/accumulator/RunNormalizerAccumulator";
 import addElevation from "./transforms/Elevation";
 import toFeatureCollection from "./transforms/FeatureCollection";
 import { formatLift } from "./transforms/LiftFormatter";
 import * as MapboxGLFormatter from "./transforms/MapboxGLFormatter";
 import { formatRun } from "./transforms/RunFormatter";
-import { formatSkiArea, InputSkiAreaType } from "./transforms/SkiAreaFormatter";
+import { InputSkiAreaType, formatSkiArea } from "./transforms/SkiAreaFormatter";
+
 import {
-  addSkiAreaSites,
   SkiAreaSiteProvider,
+  addSkiAreaSites,
 } from "./transforms/SkiAreaSiteProvider";
 import {
   accumulate,
@@ -24,77 +24,88 @@ import {
   map,
   mapAsync,
 } from "./transforms/StreamTransforms";
+import { RunNormalizerAccumulator } from "./transforms/accumulator/RunNormalizerAccumulator";
 
 export default async function prepare(paths: DataPaths, config: Config) {
   const siteProvider = new SkiAreaSiteProvider();
   siteProvider.loadSites(paths.input.osmJSON.skiAreaSites);
 
-  await Promise.all(
-    [
-      merge([
-        readGeoJSONFeatures(paths.input.geoJSON.skiAreas).pipe(
-          flatMap(formatSkiArea(InputSkiAreaType.OPENSTREETMAP_LANDUSE))
-        ),
-        Readable.from(siteProvider.getGeoJSONSites()),
-        readGeoJSONFeatures(paths.input.geoJSON.skiMapSkiAreas).pipe(
-          flatMap(formatSkiArea(InputSkiAreaType.SKIMAP_ORG))
-        ),
-      ])
-        .pipe(toFeatureCollection())
-        .pipe(
-          createWriteStream(
-            config.arangoDBURLForClustering
-              ? paths.intermediate.skiAreas
-              : paths.output.skiAreas
-          )
-        ),
+  console.log("Processing ski areas...");
 
-      readGeoJSONFeatures(paths.input.geoJSON.runs)
-        .pipe(flatMap(formatRun))
-        .pipe(map(addSkiAreaSites(siteProvider)))
-        .pipe(accumulate(new RunNormalizerAccumulator()))
-        .pipe(
-          mapAsync(
-            config.elevationServerURL
-              ? addElevation(config.elevationServerURL)
-              : null,
-            10
-          )
+  await StreamToPromise(
+    merge([
+      readGeoJSONFeatures(paths.input.geoJSON.skiAreas).pipe(
+        flatMap(formatSkiArea(InputSkiAreaType.OPENSTREETMAP_LANDUSE))
+      ),
+      Readable.from(siteProvider.getGeoJSONSites()),
+      readGeoJSONFeatures(paths.input.geoJSON.skiMapSkiAreas).pipe(
+        flatMap(formatSkiArea(InputSkiAreaType.SKIMAP_ORG))
+      ),
+    ])
+      .pipe(toFeatureCollection())
+      .pipe(
+        createWriteStream(
+          config.arangoDBURLForClustering
+            ? paths.intermediate.skiAreas
+            : paths.output.skiAreas
         )
-        .pipe(toFeatureCollection())
-        .pipe(
-          createWriteStream(
-            config.arangoDBURLForClustering
-              ? paths.intermediate.runs
-              : paths.output.runs
-          )
-        ),
+      )
+  );
 
-      readGeoJSONFeatures(paths.input.geoJSON.lifts)
-        .pipe(flatMap(formatLift))
-        .pipe(map(addSkiAreaSites(siteProvider)))
-        .pipe(
-          mapAsync(
-            config.elevationServerURL
-              ? addElevation(config.elevationServerURL)
-              : null,
-            10
-          )
+  console.log("Processing runs...");
+
+  await StreamToPromise(
+    readGeoJSONFeatures(paths.input.geoJSON.runs)
+      .pipe(flatMap(formatRun))
+      .pipe(map(addSkiAreaSites(siteProvider)))
+      // write stream here
+      // do topo conversion in a separate command
+      // then open the topojson separately and normalize
+      .pipe(accumulate(new RunNormalizerAccumulator()))
+      .pipe(
+        mapAsync(
+          config.elevationServerURL
+            ? addElevation(config.elevationServerURL)
+            : null,
+          10
         )
-        .pipe(toFeatureCollection())
-        .pipe(
-          createWriteStream(
-            config.arangoDBURLForClustering
-              ? paths.intermediate.lifts
-              : paths.output.lifts
-          )
-        ),
-    ].map((stream) => {
-      return StreamToPromise(stream);
-    })
+      )
+      .pipe(toFeatureCollection())
+      .pipe(
+        createWriteStream(
+          config.arangoDBURLForClustering
+            ? paths.intermediate.runs
+            : paths.output.runs
+        )
+      )
+  );
+
+  console.log("Processing lifts...");
+
+  await StreamToPromise(
+    readGeoJSONFeatures(paths.input.geoJSON.lifts)
+      .pipe(flatMap(formatLift))
+      .pipe(map(addSkiAreaSites(siteProvider)))
+      .pipe(
+        mapAsync(
+          config.elevationServerURL
+            ? addElevation(config.elevationServerURL)
+            : null,
+          10
+        )
+      )
+      .pipe(toFeatureCollection())
+      .pipe(
+        createWriteStream(
+          config.arangoDBURLForClustering
+            ? paths.intermediate.lifts
+            : paths.output.lifts
+        )
+      )
   );
 
   if (config.arangoDBURLForClustering) {
+    console.log("Clustering ski areas...");
     await clusterSkiAreas(
       paths.intermediate,
       paths.output,
@@ -102,6 +113,8 @@ export default async function prepare(paths: DataPaths, config: Config) {
       config.geocodingServer
     );
   }
+
+  console.log("Formatting for maps...");
 
   await Promise.all(
     [FeatureType.SkiArea, FeatureType.Lift, FeatureType.Run].map((type) => {
@@ -113,4 +126,6 @@ export default async function prepare(paths: DataPaths, config: Config) {
       );
     })
   );
+
+  console.log("Done preparing");
 }
